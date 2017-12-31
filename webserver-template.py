a7b5989f1e82f6a3dd2cf069ce016b08ba56c057
Scott A. Beeker <sbeeker@ix.netcom.com>
2017-12-31 01:25:28 -0500

    Updated for Jenkins.

---
+++ webserver-template.py
@@ -0,0 +1,54 @@
+from troposphere import Ref, Template, Parameter, Output, Join, GetAtt, Base64
+import troposphere.ec2 as ec2
+
+t = Template()
+
+# Security Group
+# AMI ID and instance typ
+# SSH key pair
+
+sg = ec2.SecurityGroup("LampSg")
+sg.GroupDescription = "Allow access through ports 22 and 80 to the WebServer"
+sg.SecurityGroupIngress = [
+	ec2.SecurityGroupRule(IpProtocol = "tcp", FromPort = "22", ToPort = "22", CidrIp = "0.0.0.0/32"),
+	ec2.SecurityGroupRule(IpProtocol = "tcp", FromPort = "80", ToPort = "80", CidrIp = "0.0.0.0/32")
+]
+t.add_resource(sg)
+
+# Lets add the SSH Key Pair as a Parameter
+keypair = t.add_parameter(Parameter(
+	"KeyName",
+	Description = "Name of the SSH key pair that will be used to access the instance",
+	Type = "String"
+	))
+
+instance = ec2.Instance("Webserver")
+instance.ImageId = "ami-55ef662f"
+instance.InstanceType = "t2.micro"
+instance.SecurityGroups = [Ref(sg)]
+instance.KeyName = Ref(keypair)
+
+t.add_resource(instance)
+
+t.add_output(Output(
+	"InstanceAccess",
+	Description = "Command to use instance using SSH",
+	Value = Join("", ["ssh -i ~/.ssh/LampKey.pem ec2_user@", GetAtt(instance,"PublicDnsName")])
+	   ))
+t.add_output(Output(
+	"WebUrl",
+	Description = "The Url of the Web Server",
+	Value = Join("", ["http://", GetAtt(instance,"PublicDnsName")])
+	   ))
+
+ud = Base64(Join('\n',
+	[  
+		"#!/bin/bash", 
+		"sudo yum -y install httpd", 
+		"sudo echo '<html><body><h1>Welcome to DevOps on AWS</h1></body></html>' > /var/www/html/test.html", 
+		"sudo service httpd start", 
+		"sudo chkconfig httpd on"  
+	]))
+instance.UserData = ud
+
+print(t.to_json())
