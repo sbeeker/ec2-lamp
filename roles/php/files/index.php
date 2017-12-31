3c6ff4c9e5bf75543111a37797b71744b4d07c62
Scott A. Beeker <sbeeker@ix.netcom.com>
2017-12-30 23:40:59 -0500

    Testing Ansible Pull (Corrected).

--- roles/php/files/index.php
+++ roles/php/files/index.php
@@ -5,6 +5,6 @@
 </head>
 <body>
 	<h1><?php echo "Welcome to DevOps on AWS. This text was generated using PHP"; ?></h1>
-	<h2><?php echo "This shows the pull worked!"</h2>
+	<h2><?php echo "This shows the pull worked!"; ?></h2>
 </body>
 </html>
